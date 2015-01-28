14aae93834206cac78f505cd892b8e8ddb25569d
Shane <carrollshaned@gmail.com>
2015-01-27 21:12:43 -0600

    Continued working on SVM (SMO)
    Continued to implement the SMO algorithm for the Svm class.

--- MachineLearning/SvmClassifier.cs
+++ MachineLearning/SvmClassifier.cs
@@ -10,49 +10,56 @@ namespace MachineLearning
     {
         protected readonly Random Random = new Random(DateTime.Now.Millisecond);
 
+        // number of input elements
         private int _n;
-        private double[] _a;
+
+        // array of lagrangian multipliers ("alphas")
+        private Dictionary<int, double> _a;
+        
+        // target value vector
         private double[] _y;
+
+        // array of input vectors
         private Vector<double>[] _x;
+
+        // array of (human friendly) class labels that correspond to the target value vector (_y)
         private string[] _classLabels;
+
+        // a dictionary that maps the class labels to their target value representation ( "classOne" -> -1, "classTwo" -> 1 )
         private readonly Dictionary<string, double> _labelValueMap = new Dictionary<string, double>();
         
         // determines the `accuracy` of the approximation function 
         private double _epsilon = .5;
         
         // the threshold value
-        private double _b = 0.0;
+        private double _b;
+
+        private double c = 0.23675;
 
         private const double VerySmallValue = .00000001;
+
+        private const double _tolerance = 0.001;
 
         public void Train(Vector<double>[] trainingData, string[] classLabels)
         {
             Initialize(trainingData, classLabels);
-            while (UpdateNextAlphasUntilOptimized())
-            {
-                _ix1++;
-                _ix2++;
-            }
         }
 
-        private int _ix1 = 0;
-        private int _ix2 = 1;
 
-        private bool UpdateNextAlphasUntilOptimized()
+        private bool UpdateNextAlphasUntilOptimized(int ix1, int ix2)
         {
-            double a1_old = _a[_ix1];
-            double a2_old = _a[_ix2];
+            double a1_old = _a[ix1];
+            double a2_old = _a[ix2];
 
-            double y1 = _y[_ix1];
-            double y2 = _y[_ix2];
+            double y1 = _y[ix1];
+            double y2 = _y[ix2];
 
-            var x1 = _x[_ix1];
-            var x2 = _x[_ix2];
+            var x1 = _x[ix1];
+            var x2 = _x[ix2];
 
-            var e1 = L(_ix1) - y1;
-            var e2 = L(_ix2) - y2;
+            var e1 = L(ix1) - y1;
+            var e2 = L(ix2) - y2;
 
-            double c = (y1 * a1_old) + (y2 * a2_old);
             double s = y1 * y2;
 
             bool yEqual = y1.IsEqualTo(y2);
@@ -78,20 +85,21 @@ namespace MachineLearning
                 {
                     a2 = high;
                 }
-                //_a[_ix2] = a2;
             }
             else
             {
-                _a[_ix2] = low;
-                double Lobj = L(_ix2);
-                _a[_ix2] = high;
-                double Hobj = L(_ix2);
+                _a[ix2] = low;
+                double lobj = L(ix2);
+                _a[ix2] = high;
+                double hobj = L(ix2);
 
-                if (Lobj > (Hobj + _epsilon))
+                _a[ix2] = a2_old;
+
+                if (lobj > (hobj + _epsilon))
                 {
                     a2 = low;
                 }
-                else if (Lobj < (Hobj - _epsilon))
+                else if (lobj < (hobj - _epsilon))
                 {
                     a2 = high;
                 }
@@ -113,24 +121,49 @@ namespace MachineLearning
                 return false;
             }
             a1 = a1_old + (s*(a2_old - a2));
-            _a[_ix1] = a1;
-            _a[_ix2] = a2;
-            //
-                /*a1 + a2_old + c 
-                - (.5*(((y1*y1*(x1.DotProduct(x1))*a1)+(y2*y2*(x2.DotProduct(x2))*a2_old)+(2*(y1*y2*x1.DotProduct(x2)*a1*a2_old))) 
-                + (2*()))
-                )*/
+            _a[ix1] = a1;
+            _a[ix2] = a2;
 
-
+            // update the threshold to match the changes in alpha
+            var b1 = _b - e1 - (y1*(a1 - a1_old)*(x1.DotProduct(x1))) - (y2*(a2 - a2_old)*(x1.DotProduct(x2)));
+            var b2 = _b - e2 - (y1*(a1 - a1_old)*(x1.DotProduct(x2))) - (y2*(a2 - a2_old)*(x2.DotProduct(x2)));
+            _b = (0 < a1 && a1 < c) ? b1 : (0 < a2 && a2 < c) ? b2 : (b1 + b2)/2;
 
             return true;
         }
 
+        private bool ExamineExample(int ix2)
+        {
+            var y2 = _y[ix2];
+            var a2 = _a[ix2];
+            var e2 = L(ix2) - y2;
+            var r2 = e2*y2;
+            if ((r2 < -_tolerance && a2 < c) || (r2 > _tolerance && a2 > 0))
+            {
+                var nonZeroNonCAlphas = _a.Where(a => a.Value.IsNonZero() && !a.Value.IsEqualTo(c)).ToList();
+                if (nonZeroNonCAlphas.Count > 1)
+                {
+                    int k, ix1;
+                    float tmax;
+                    for (k = 0, tmax = 0f, ix1 = -1; k < _n; k++)
+                    {
+                        if (_a[k] > 0 && _a[k] < c)
+                        {
+                            //float e2, temp;
+
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
         private void Initialize(Vector<double>[] trainingData, string[] classLabels)
         {
+            _b = 0;
             _x = trainingData;
             _n = _x.Length;
-            _a = new double[_n];
+            _a = new Dictionary<int, double>(_n + 1);
             _y = new double[_n];
 
             if (_n < 2)
@@ -142,8 +175,7 @@ namespace MachineLearning
             var classes = _classLabels.Distinct().ToArray();
             if (classes.Length > 2)
             {
-                throw new NotSupportedException(
-                    "SVMs with more than 2 classes have not yet been implemented. For now, please use only 2 classes.");
+                throw new NotSupportedException("SVMs with more than 2 classes have not yet been implemented. For now, please use only 2 classes.");
             }
 
             var classOne = classLabels[0];
@@ -154,7 +186,7 @@ namespace MachineLearning
 
             for (int i = 0; i < _n; i++)
             {
-                _a[i] = Random.NextDouble();
+                _a[i] = 0;
                 _y[i] = _labelValueMap[classLabels[i]];
             }
         }
@@ -194,7 +226,7 @@ namespace MachineLearning
             return sum;
         }
 
-        private Vector<double> W()
+        /*private Vector<double> W()
         {
             throw new NotImplementedException("I'm not sure this is correct");
             var w = new Vector<double>(_n);
@@ -203,7 +235,7 @@ namespace MachineLearning
                 w[i] = _x[i].Norm() *_a[i] *_y[i];
             }
             return w;
-        }
+        }*/
 
         protected abstract double Kernel(Vector<double> xi, Vector<double> xj);
     }
